c617bc6de6eac50e48303a1220f5f6733fa7edb8
wzy <wzy@qq.com>
2023-06-19 11:26:15 +0800

    消息队列接收数据完成，接下来就是处理了

--- zt-question/src/main/java/com/rkzt/question/service/Consumer.java
+++ zt-question/src/main/java/com/rkzt/question/service/Consumer.java
@@ -15,6 +15,9 @@ public class Consumer {
         System.out.println("消费者接收到了object.queue的消息：【" + msg + "】");
         int jiluid = (int)msg.get("jiluid");
         List<Question> list = (List<Question>) msg.get("question");
+        //1、遍历根据题目id查询正确答案与用户提交的答案进行比对，如果一样就加分，不一样就减分
+
+        //2、将用户最后的分数根据记录id提交到exam_record表中
 
     }
 }
